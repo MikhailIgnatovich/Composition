05a9185a10302ce51ac63bdc9c785f23e37246d1
MikhailIgnatovich <bulich198922@gmail.com>
2021-09-14 22:33:29 +0300

    Реализовал GameRepositoryIMPL.kt

--- app/src/main/java/com/bulich/misha/composition/domain/useCases/GenerateQuestionUseCase.kt
+++ app/src/main/java/com/bulich/misha/composition/domain/useCases/GenerateQuestionUseCase.kt
@@ -1,4 +1,16 @@
 package com.bulich.misha.composition.domain.useCases
 
-class GenerateQuestionUseCase {
+import com.bulich.misha.composition.domain.entity.Question
+import com.bulich.misha.composition.domain.repository.GameRepository
+
+class GenerateQuestionUseCase(private val repository: GameRepository) {
+
+    operator fun invoke(maxSumValue: Int): Question {
+        return repository.generateQuestion(maxSumValue, COUNT_OF_OPTIONS)
+    }
+
+    private companion object {
+
+        private const val COUNT_OF_OPTIONS = 6
+    }
 }
--- app/src/main/java/com/bulich/misha/composition/domain/useCases/GetGameSettingsUseCase.kt
+++ app/src/main/java/com/bulich/misha/composition/domain/useCases/GetGameSettingsUseCase.kt
@@ -1,4 +1,12 @@
 package com.bulich.misha.composition.domain.useCases
 
-class GetGameSettingsUseCase {
+import com.bulich.misha.composition.domain.entity.GameSettings
+import com.bulich.misha.composition.domain.entity.Level
+import com.bulich.misha.composition.domain.repository.GameRepository
+
+class GetGameSettingsUseCase(private val repository: GameRepository) {
+
+    operator fun invoke(level: Level): GameSettings {
+        return repository.getGameSettings(level)
+    }
 }
